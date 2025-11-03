997674b4f74b0b714100655e6f855ff58829c8b4
maxbarrenechea <mbarrenechea27039@columbushs.com>
2025-11-03 13:51:10 -0500

    Update Lab1_2_DataTypes.js

--- Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
+++ Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
@@ -3,15 +3,31 @@
 */
 
 // TODO 1: Create variables age (34) and price (12.99), print both
+let age = 34;
+console.log("Age:", age);
+
+let price = 12.99;
+console.log("Price:", price);
+
 // TODO 1b: Divide "Sandra" by 2 and print result (expect NaN)
-
+let sandra = "Sandra";
+console.log(sandra /2);
+console.log("Sandra" /2);
 // TODO 2: Print basic math ops: + - * /
+console.log("2 + 2 =", 2+2)
+console.log("4 -2 =", 4-2)
+console.log("3 * 2 =", 3*2)
+console.log("6/ 2 =", 6/2)
 // TODO 2b: Use exponentiation (2 ** 3) and modulo (7 % 2)
-
+console.log("2 ** 3 =", 2 ** 3);
 // TODO 3: Assignment operators with score variable
-
+console.log("7 % 2, =", 7 % 2);
 // TODO 4: Expressions with operator precedence
-
+let score =10;
+score +=5;
+score -=3;
+score *=8
+score /=2;
 // TODO 5: Strings (single, double, backticks)
 
 // TODO 6: String properties and methods
