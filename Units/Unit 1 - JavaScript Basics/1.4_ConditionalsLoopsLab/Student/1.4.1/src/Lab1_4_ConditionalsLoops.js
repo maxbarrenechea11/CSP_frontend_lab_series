474c2b146164e8ee040ff26b12919034274c7391
maxbarrenechea <mbarrenechea27039@columbushs.com>
2025-11-12 10:48:29 -0500

    commiting 1.4

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.1/src/Lab1_4_ConditionalsLoops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.1/src/Lab1_4_ConditionalsLoops.js
@@ -3,10 +3,90 @@
 */
 
 // TODO 1: if..else with age discounts
+let age = 15;
+if (age <= 16) {
+    console.log("Teen Discount");
+} else if (age >= 65) {
+    console.log("Senior Discount");
+} else {
+    console.log("No Discount");
+}
 
+let weight = 180;
+if (weight <= 150) {
+    console.log("You are Slim");
+} else if (weight <= 190) {
+    console.log("You are in shape");
+} else{
+    console.log("You need to start working out");
+}
 // TODO 2: Nested if with two numbers
 
+let number1 = 10;
+let number2 = 20;
+
+if (typeof number1 == 'Number' && typeof number2 == 'Number') {
+
+if (number1 === number2) {
+    console.log ("Both numbers are equal")
+} else if (number1 > number2) {
+    console.log("Number 1 is greater than Number 2");
+} else { 
+    console.log("Number 1 is smaller")
+}
+
+}
 // TODO 3: Switch statement for language greeting
+
+let language = "French"
+switch (language) {
+    case "Spanish":
+        console.log("Hola, mundo");
+        break;
+    case "French":
+        console.log("Bonjour, le monde");
+        break;
+    default:
+        console.log("Hello, world");
+        break;
+}
+
+let stock = "APPL"
+switch(stock) {
+    case "APPL":
+        console.log("Apple Inc.");
+        break;
+    case "TSLA":
+        console.log("Tesla, Inc.");
+        break;
+    case "NVDA":
+        console.log("NVIDIA Corporation");
+        break;
+    case "XOM":
+        console.log("Exxon Mobil Corporation");
+        break;
+    case "CVX":
+        console.log("Chevron Corporation");
+        break;
+    case "SHEL":
+        console.log("Shell plc");
+        break;
+    case "BLK":
+        console.log("BlackRock, Inc.");
+        break;
+    case "VTI":
+        console.log("Vanguard Total Stock Market ETF");
+        break;
+    case "JPM":
+        console.log("JPMorgan Chase & Co.");
+        break;
+    
+
+    
+}
+
+
+     
 
 // TODO 4: While loop (1 to 10)
 
